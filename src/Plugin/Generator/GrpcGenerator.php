6e989f4b1158205c44ae0af176a45ea872993469
kafkiansky <vadimzanfir@gmail.com>
2026-03-03 10:24:36 +0300

    Change server stream server api

--- src/Plugin/Generator/GrpcGenerator.php
+++ src/Plugin/Generator/GrpcGenerator.php
@@ -254,12 +254,11 @@ PHP,
                 $interfaceMethod
                     ->setParameters([
                         new Parameter('request')->setType($in->fqcn),
-                        new Parameter('stream')->setType('Server\ServerStreamChannel'),
                         new Parameter('md')->setType('Metadata'),
                         new Parameter('cancellation')->setType('Cancellation'),
                     ])
-                    ->setReturnType('void')
-                    ->addComment("{$phpdocPrefix}@param Server\\ServerStreamChannel<{$in->fqcn}, {$out->fqcn}> \$stream");
+                    ->setReturnType('iterable')
+                    ->addComment("@return iterable<array-key, {$out->fqcn}>");
             } else {
                 $interfaceMethod
                     ->setParameters([
