bb872a9f2144c1a81d053af0c0de9a86198c7164
kafkiansky <vadimzanfir@gmail.com>
2026-04-09 09:32:37 +0300

    chore: grpc deprecations

--- src/Plugin/Parser/ServiceDescriptor.php
+++ src/Plugin/Parser/ServiceDescriptor.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Thesis\Protoc\Plugin\Parser;
 
+use Google\Protobuf\ServiceOptions;
 use Thesis\Protoc\Plugin\Comment;
 
 /**
@@ -19,5 +20,6 @@ final readonly class ServiceDescriptor
         public string $path,
         public array $methods,
         public ?Comment $comment = null,
+        public ?ServiceOptions $options = null,
     ) {}
 }
